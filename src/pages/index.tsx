4ecb1dcb12bc1b26bbb897457bda2c4fd27d1f0a
quynk <khaquy12a2@gmail.com>
2025-06-08 10:02:07 +0700

    set up custom field mui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 'use client'
+import { Box } from '@mui/material'
 import Head from 'next/head'
+import CustomTextField from 'src/components/text-field'
 
 export default function Home() {
   return (
@@ -10,7 +12,9 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <h1>Hello world!</h1>
+      <Box sx={{ margin: 6, width: '200px' }}>
+        <CustomTextField id='outlined-multiline-flexible' label='Multiline' />
+      </Box>
     </>
   )
 }
